f9e6d72d4fac958d9253557977ebfa8c75d67651
muqushushu <muqushushu@163.com>
2021-03-14 19:13:19 +0800

    学习

---
+++ app/src/main/java/com/test/helloworld/MainActivity.java
@@ -0,0 +1,21 @@
+package com.test.helloworld;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        boolean age = new TestBean().isAge();
+
+    }
+
+
+
+}
